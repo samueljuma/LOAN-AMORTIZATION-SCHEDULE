d236793d8f94dbd75b6ac736b9f6e0cef1cc3a4e
samueljuma <samueljuma1024@gmail.com>
2021-04-22 10:08:30 +0300

    calculations method

--- SourceCode/src/loan_repayment/LoanRepayment.java
+++ SourceCode/src/loan_repayment/LoanRepayment.java
@@ -1,15 +1,10 @@
 package loan_repayment;
 public class LoanRepayment {
     static Object[][] data;
-
     public static void Calculations(double loanAmount, double interestRate, double loanPeriod) {
         double paymentAmount, paymentPrint, principalAmount, principalPrint;
         double interest, interestPrint, loanOutstandingBalance, balancePrint;
-        //Calculations
-        int totalNumberOfPayments = (int) loanPeriod; int count = 0;
-        data = new Object[totalNumberOfPayments][5];
 
     }
-
     
 }
