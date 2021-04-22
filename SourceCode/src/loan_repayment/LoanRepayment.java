30edbed3c854d16d0e126ebc21f9f18cff1d4eb6
samueljuma <samueljuma1024@gmail.com>
2021-04-22 10:09:56 +0300

    calculation of payment amount

--- SourceCode/src/loan_repayment/LoanRepayment.java
+++ SourceCode/src/loan_repayment/LoanRepayment.java
@@ -4,7 +4,17 @@ public class LoanRepayment {
     public static void Calculations(double loanAmount, double interestRate, double loanPeriod) {
         double paymentAmount, paymentPrint, principalAmount, principalPrint;
         double interest, interestPrint, loanOutstandingBalance, balancePrint;
-
+        //Calculations
+        int totalNumberOfPayments = (int) loanPeriod; int count = 0;
+        data = new Object[totalNumberOfPayments][5];
+        do {
+            //  payment amount
+            data[count][0] = count + 1; // stores value
+            paymentAmount = ((interestRate*loanAmount)/(1-(Math.pow((1+interestRate),(-(loanPeriod))))));
+            paymentPrint = Math.round(paymentAmount * 100.0) / 100.0;
+            data[count][1] = paymentPrint; // stores value
+           
+        } while (count < totalNumberOfPayments);
     }
     
 }
